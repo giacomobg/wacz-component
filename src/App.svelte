94b6a4b601326e060b98774baa27d93752f7669a
giacomobg <pepperroni@hotmail.co.uk>
2023-08-30 19:28:20 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+
+  // import * from './lib/replay/ui.js';
+  import Embed from './lib/Embed.svelte';
+
+  let filename = 'mono-county';
+  let visible = false;
+
+  function handleClick() {
+    // load in mono-county-web-01.json
+    visible = true;
+  };
+
+  
+</script>
+
+<Embed bind:visible={visible} {filename}/>
+
+<main>
+
+  <button id="show-info" on:click={handleClick}>Click this to trigger bringing up the Mono County embed</button>
+
+
+
+
+</main>
+
+<style>
+
+  main {
+    display: block;
+    position: absolute;
+    width: 800px;
+    height: 800px;
+    border: 4px black solid;
+  }
+
+  button {
+    display: block;
+    margin: 0 auto;
+  }
+
+
+
+  /* html, body {
+    width: 100%;
+    height: 100%;
+  } */
+</style>
---
+++ src/lib/Embed.svelte
@@ -0,0 +1,66 @@
+<script>
+
+    export let visible;
+    export let filename;
+
+    // let json_filename = '/assets/'+filename+'01-content.json';
+    // import data from json_filename;
+
+    function closeBox() {
+        visible = false;
+    }
+
+
+</script>
+
+<div id="wacz-popup" class={visible ? 'visible' : ''}
+on:click={closeBox}>
+
+<p class='info-title'>Mono County</p>
+
+  <replay-web-page id="embed" source={"/assets/" + filename + "-web-01.wacz"} embed="replay-only" url="https://monocounty.ca.gov/cao/page/mono-county-board-supervisors-adopts-resolution-recognizing-racism-public-health-crisis">
+  </replay-web-page>
+
+  <div id="info">
+    <p>Webpage</p>
+        <p><a href="https://monocounty.ca.gov/cao/page/mono-county-board-supervisors-adopts-resolution-recognizing-racism-public-health-crisis">https://monocounty.ca.gov/cao/page/mono-county-board-supervisors-adopts-resolution-recognizing-racism-public-health-crisis</a></p>
+  </div>
+
+</div>
+
+<style>
+
+  #wacz-popup {
+    z-index: -1;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    color: white;
+    background-color: #333;
+    opacity: 0;
+    transition: opacity 1s, z-index 1s;
+  }
+
+  #wacz-popup.visible {
+    z-index: 10;
+    opacity:1;
+  }
+
+  #info, replay-web-page {
+    float: left;
+    /* display: inline; */
+    /* position: relative; */
+    width: calc(50% - 10px);
+  }
+
+  #info {
+    margin: 10px;
+  }
+
+  .info-title {
+    font-size: 18px;
+    font-weight: 700;
+  }
+</style>
