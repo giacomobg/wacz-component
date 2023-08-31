a9696cc8e7f4b6addaf4ebb4554f9441ca63e5db
giacomobg <pepperroni@hotmail.co.uk>
2023-08-31 03:53:40 +0200

    Hook up json

--- src/lib/Embed.svelte
+++ src/lib/Embed.svelte
@@ -3,12 +3,17 @@
     export let visible;
     export let filename;
 
-    // async function import_json() {
-        const json_content = fetch('https://giacomobg.github.io/wacz-component/dist/assets/'+filename+'.content.json')['contentMetadata'];
+    let json_content;
+
+    async function import_json() {
+        let response = await fetch('https://giacomobg.github.io/wacz-component/dist/assets/'+filename+'.content.json');
+        let response_json = await response.json();
+        const json_content = response_json['contentMetadata']
         console.log(json_content);
-        const json = fetch('https://giacomobg.github.io/wacz-component/dist/assets/'+filename+'.json');
-    // }
-    // import_json();
+        url = json_content['private']['crawl_config']['config']['seeds'][0]['url'];
+        const json = await fetch('https://giacomobg.github.io/wacz-component/dist/assets/'+filename+'.json');
+    }
+    import_json();
     // import data from json_filename;
     // let json_filename = '/assets/'+filename+'.json';
     // import data from json_filename;
@@ -17,6 +22,11 @@
         visible = false;
     }
 
+    let url;
+    $: console.log(json_content);
+    $: if (json_content) {
+        console.log(url);
+    } 
 
 </script>
 
@@ -25,12 +35,12 @@ on:click={closeBox}>
 
 <p class='info-title'>Mono County</p>
 
-  <replay-web-page id="embed" source={"/assets/" + filename} embed="replay-only" url="https://monocounty.ca.gov/sites/default/files/fileattachments/board_of_supervisors/meeting/30464/r20-93.pdf">
+  <replay-web-page id="embed" source={"/assets/" + filename} embed="replay-only" {url}>
   </replay-web-page>
 
   <div id="info">
     <p>Webpage</p>
-        <p><a href={'https://monocounty.ca.gov/sites/default/files/fileattachments/board_of_supervisors/meeting/30464/r20-93.pdf'}>https://monocounty.ca.gov/sites/default/files/fileattachments/board_of_supervisors/meeting/30464/r20-93.pdf</a></p>
+        <p><a href={url}>{url}</a></p>
   </div>
 
 </div>
