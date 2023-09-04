ae3fa92f71d15c65ab86cb95292b1cf2d24be633
giacomobg <pepperroni@hotmail.co.uk>
2023-09-05 01:51:04 +0200

    Forgot to add dist to end of base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://giacomobg.github.io/wacz-component/",
+  base: "https://giacomobg.github.io/wacz-component/dist",
   plugins: [
     svelte({
       compilerOptions: {
