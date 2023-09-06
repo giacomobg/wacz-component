696037e201c38f1cfc2d168ea819c09c188b1573
giacomobg <pepperroni@hotmail.co.uk>
2023-09-06 13:24:51 +0200

    Play with paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // when compiling for production, this is useful to make sure the links to index files are correct
   base: "https://giacomobg.github.io/wacz-component/dist",
   plugins: [
     svelte({
